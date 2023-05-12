59ec0f80697981ce6510aabd9595db6c612eaa00
YoelEigner <35924447+YoelEigner@users.noreply.github.com>
2023-05-12 21:32:27 +0300

    cleanup

--- js/login.js
+++ js/login.js
@@ -9,12 +9,16 @@ const Login = async (username, password) => {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username: username, password: password })
         });
-        if (!response.ok) { throw new Error('Network response was not ok'); }
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
         const jsonResponse = await response.json();
         sessionStorage.setItem('user', JSON.stringify(jsonResponse))
         window.location.href = 'main.html';
 
     } catch (error) {
+        alert('Incorrect username or password')
+
         console.error('There was a problem with the network request:', error);
     }
 }
